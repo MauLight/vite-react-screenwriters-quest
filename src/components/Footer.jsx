32aa67a2b83015f96aebacce17e81978614f4150
MauLight <maulisseluz@gmail.com>
2023-05-21 09:53:53 -0400

    links and form added to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,76 +3,63 @@ import { Button, Grid, Input, Text, Textarea, Table } from "@nextui-org/react";
 
 export default function Footer() {
   return (
-    <div className="columns-2 px-20">
-      <Grid css={{ padding: "50px" }}>
-        <div className="w-[100%]">
-          <Text h1 size={35} color="black" css={{ fontFamily: "Josefin Sans" }}>SCREENWRITERS' QUEST</Text>
+    <div className="grid grid-cols-2 px-20">
+      <div className="p-[50px]">
+        <div className="w-[100%] pb-[50px]">
+          <Text h1 size={35} color="black" weight="bold" css={{ fontFamily: "Josefin Sans" }}>SCREENWRITERS' QUEST</Text>
           <Text p size={25} color="black" css={{ fontFamily: "Raleway" }} >Your next feature starts here.</Text>
         </div>
-        <div>
-          <Table
-            lined
-            headerLined
-            shadow={false}
-            selectionMode="multiple"
-            aria-label="Example static collection table"
-            css={{
-              height: "auto",
-
-            }}
-          >
-            <Table.Header>
-              <Table.Column>NAME</Table.Column>
-              <Table.Column>ROLE</Table.Column>
-              <Table.Column>STATUS</Table.Column>
-            </Table.Header>
-            <Table.Body>
-              <Table.Row key="1">
-                <Table.Cell>Tony Reichert</Table.Cell>
-                <Table.Cell>CEO</Table.Cell>
-                <Table.Cell>Active</Table.Cell>
-              </Table.Row>
-              <Table.Row key="2">
-                <Table.Cell>Zoey Lang</Table.Cell>
-                <Table.Cell>Technical Lead</Table.Cell>
-                <Table.Cell>Paused</Table.Cell>
-              </Table.Row>
-              <Table.Row key="3">
-                <Table.Cell>Jane Fisher</Table.Cell>
-                <Table.Cell>Senior Developer</Table.Cell>
-                <Table.Cell>Active</Table.Cell>
-              </Table.Row>
-              <Table.Row key="4">
-                <Table.Cell>William Howard</Table.Cell>
-                <Table.Cell>Community Manager</Table.Cell>
-                <Table.Cell>Vacation</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
+        <div className="flex justify-center">
+          <div className="grid grid-cols-3 gap-x-48">
+            <div className="flex flex-col gap-y-10">
+              <a className="nav-item">ABOUT</a>
+              <a className="nav-item">PORTFOLIOS</a>
+              <a className="nav-item">REVIEWS</a>
+            </div>
+            <div className="flex flex-col gap-y-10">
+              <a className="nav-item">TEAM</a>
+              <a className="nav-item">PRICING</a>
+              <a className="nav-item">BLOG</a>
+            </div>
+            <div className="flex flex-col gap-y-10">
+              <a className="nav-item">
+                <i className="text-xl fa-solid fa-magnifying-glass"></i>
+              </a>
+              <a className="nav-item">
+                <i className="text-xl fa-brands fa-instagram"></i>
+              </a>
+              <a className="nav-item">
+                <i className="text-xl fa-regular fa-user"></i>
+              </a>
+            </div>
+          </div>
         </div>
-      </Grid>
-      <Grid css={{ padding: "50px" }}>
-        <div className="w-[100%]">
-          <Text h1 size={35} color="black" css={{ fontFamily: "Poppins" }}>Do you have questions?</Text>
+      </div>
+      <div className="p-[50px]">
+        <div className="w-[100%] mb-5">
+          <Text h1 size={35} color="black" css={{ fontFamily: "Josefin Sans" }}>DO YOU HAVE ANY QUESTIONS?</Text>
           <Text p size={25} color="black" css={{ fontFamily: "Raleway" }} >Feel free to write us below, we'll contact you soon!</Text>
         </div>
-        <div className="mt-10 flex justify-center gap-x-5">
-          <div>
-            <Input type="text" width="370px" labelPlaceholder="Name" bordered />
+        <div className=" flex justify-center gap-x-5">
+          <div className="w-[100%]">
+            <label htmlFor="Name" className="mb-10 text-gray-400">Name</label>
+            <Input id="Name" className="w-[100%] mt-2" type="text" fullWidth clearable bordered status="default" size="xl" rounded color="primary" />
           </div>
-          <div>
-            <Input type="email" width="370px" labelPlaceholder="Email" rounded bordered />
+          <div className="w-[100%]">
+            <label htmlFor="Email" className="mb-10 text-gray-400">Message</label>
+            <Input id="Email" className="w-[100%] mt-2" type="text" fullWidth clearable bordered status="default" size="xl" rounded color="primary" />
           </div>
         </div>
-        <div className="mt-10 flex justify-center gap-x-5">
-          <Textarea fullWidth labelPlaceholder="Message" rounded bordered />
+        <div className="mt-5">
+          <label htmlFor="Message" className="mb-10 text-gray-400">Message</label>
+          <textarea id="Message" className="rounded-full h-[100px] w-[100%] border-solid border-2 border-gray-300 mt-2"></textarea>
         </div>
-        <div className="mt-10 flex justify-center gap-x-5">
-          <button className="rounded-[50px] border-solid border-2 border-black px-8 py-3 hover:bg-black hover:text-white transition-all duration-500">
+        <div className="mt-5 flex justify-center gap-x-5">
+          <button className="rounded-[50px] border-solid border-2 border-gray-300 text-gray-400 px-8 py-3 hover:bg-black hover:text-white hover:border-white transition-all duration-500">
             <p className='font-tertiary text-xl'><b>Submit</b></p>
           </button>
         </div>
-      </Grid>
+      </div>
     </div>
   )
 }
